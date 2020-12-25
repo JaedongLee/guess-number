61ef085a2c0a4a589dbe149443c4840ef4c0bc5a
邹泰然 <tairan.zou@casstime.com>
2020-12-25 21:53:36 +0800

    feat: guess number

--- src/main/java/com/thoughtworks/GuessNumber.java
+++ src/main/java/com/thoughtworks/GuessNumber.java
@@ -1,27 +1,49 @@
 package com.thoughtworks;
 
+import cn.hutool.core.util.StrUtil;
+
 public class GuessNumber {
-    private AnswerGenerator answerGenerator;
+    private final AnswerGenerator answerGenerator;
     private String answer;
 
     public GuessNumber(AnswerGenerator answerGenerator) {
         this.answerGenerator = answerGenerator;
     }
 
-    public String guess(String userInput){
-        //TODO 驱动出下面的代码
-        return null;
-    }
-
-    public void start() {
-        this.answer = answerGenerator.generate();
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         AnswerGenerator answerGenerator = new AnswerGenerator();
         GuessNumber guessNumber = new GuessNumber(answerGenerator);
         guessNumber.start();
         String guess = guessNumber.guess("1234");
+    }
+
+    public String guess(String userInput) {
+        if (StrUtil.isBlank(userInput) || userInput.length() < 4 || isDistinct(userInput)) {
+            throw new IllegalArgumentException();
+        }
+        char[] guessChars = userInput.toCharArray();
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < guessChars.length; i++) {
+            char guessChar = guessChars[i];
+            int index = answer.indexOf(guessChar);
+            if (index == i) {
+                a++;
+                continue;
+            }
+            if (index >= 0) {
+                b++;
+            }
+        }
+        return a + "A" + b + "B";
+    }
+
+    private boolean isDistinct(String userInput) {
+        return userInput.chars().distinct().count() < userInput.length();
+    }
+
+    public void start() {
+        this.answer = answerGenerator.generate();
     }
 
 }
